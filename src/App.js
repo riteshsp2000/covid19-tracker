057c075f85a8b3ec07705143d18bf37d148545b9
Ritesh Patil <riteshsp2000@gmail.com>
2020-05-26 20:07:04 +0530

    Added a component to show the intro

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import cx from 'classnames';
 
 import { Cards, Chart, CountryPicker } from './components';
 import styles from './App.module.css';
@@ -10,6 +11,29 @@ class App extends React.Component {
     country: '',
   };
 
+  renderIntro() {
+    return (
+      <div>
+        <div className={styles.content}>
+          <h5>
+            Coronavirus <span>Covid-19</span>
+          </h5>
+          <h2>World Live Tracker</h2>
+          <h4>Stay Home, Stay Safe</h4>
+          <p>
+            The best way to prevent and slow down transmission is to be well
+            informed about the Covid-19 disease. Protect yourself and others
+            from infection by washing your hands at-least 20 sec nd not touching
+            your face.
+          </p>
+        </div>
+        <div className={styles.imgDiv}>
+          <img src='./img/distancing.png' alt='' />
+        </div>
+      </div>
+    );
+  }
+
   async componentDidMount() {
     const fetchedData = await fetchData();
 
@@ -17,16 +41,19 @@ class App extends React.Component {
   }
 
   handleCountryChange = async (country) => {
-    const fetchData = await fetchData(country);
+    const countryData = await fetchData(country);
+
+    this.setState({ data: countryData, country: country });
   };
 
   render() {
-    const { data } = this.state;
+    const { data, country } = this.state;
     return (
-      <div className={styles.container}>
+      <div className='ui container'>
+        {this.renderIntro()}
         <Cards data={data} />
         <CountryPicker handleCountryChange={this.handleCountryChange} />
-        <Chart />
+        <Chart data={data} country={country} />
       </div>
     );
   }
