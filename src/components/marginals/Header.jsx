41ce329338d03f805aded0c34653d654db934ce2
Ritesh Patil <riteshsp2000@gmail.com>
2020-05-30 04:48:07 +0530

    Completed a responsive menu bar

--- src/components/marginals/Header.jsx
+++ src/components/marginals/Header.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import cx from 'classnames';
 
@@ -6,12 +6,22 @@ import styles from '../../css/Header.module.css';
 import image from '../../img/title-image.png';
 
 const Headers = () => {
+  const [menuToggle, setMenuToggle] = useState(false);
+
+  const handleMenuToggle = () => {
+    if (menuToggle) {
+      setMenuToggle(false);
+    } else {
+      setMenuToggle(true);
+    }
+  };
+
   return (
     <div className={styles.headerContainer}>
       <div className={styles.imgDiv}>
         <img src={image} alt='Icon' className={styles.headerImage} />
       </div>
-      <ul className={styles.ulHeader}>
+      <ul className={menuToggle ? styles.ulHeaderExpand : styles.ulHeader}>
         <li className={styles.navLinks}>
           <NavLink
             to='/'
@@ -42,6 +52,9 @@ const Headers = () => {
           </NavLink>
         </li>
       </ul>
+      <button className={styles.menu} onClick={() => handleMenuToggle()}>
+        <i className='fas fa-bars fa-3x'></i>
+      </button>
     </div>
   );
 };
