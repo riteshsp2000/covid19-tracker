ef95e9b3e1fa75e888ad0dadb91d0ea841c51862
Ritesh Patil <riteshsp2000@gmail.com>
2021-07-12 17:09:33 +0530

    update links

--- src/components/marginals/Footer.jsx
+++ src/components/marginals/Footer.jsx
@@ -10,19 +10,21 @@ const Footer = ({ scrollToTop }) => {
         <h5>Stay Safe</h5>
       </div>
       <div className={cx(styles.box, styles.icons)}>
-        <a href='https://github.com/shreyasmali'>
+        <a href='https://github.com/dewanggogte'>
           <i className={cx(styles.fas, 'fab fa-github fa-2x')}></i>
         </a>
         <a href='https://covid19.mathdro.id/api'>
           <i className={cx(styles.fas, 'fas fa-server fa-2x')}></i>
         </a>
-        <a href='https://github.com/shreyasmali/covid19-tracker'>
+        <a href='https://github.com/dewanggogte/Covid-19-Tracker'>
           <i className={cx(styles.fas, 'fas fa-code fa-2x')}></i>
         </a>
       </div>
       <div className={cx(styles.box, styles.hello)}>
         <h5>Say Hello</h5>
-        <h5>shreyasmali001@gmail.com</h5>
+        <a href="mailto:dewanggogte@gmail.com" style={{textDecoration:'none', color: 'white'}}>
+        <h5>dewanggogte@gmail.com</h5>
+        </a>
       </div>
       <i
         onClick={() => scrollToTop()}
